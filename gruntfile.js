5d37f57107d77bccec554a1a9d67d81202620abc
yussan <monitor.yussan@gmail.com>
2024-05-10 08:50:58 +0700

    reinit

--- gruntfile.js
+++ gruntfile.js
@@ -1,145 +1,163 @@
-module.exports = grunt => {
+module.exports = (grunt) => {
   grunt.initConfig({
     // pug task
     pug: {
       compile: {
         options: {
           data: {
-            debug: true
+            debug: true,
           },
-          pretty: false
+          pretty: false,
         },
-        files: [{
-          src: '*.pug',
-          cwd: 'src/pug/',
-          dest: './',
-          expand: true,
-          ext: '.html'
-        }]
-      }
+        files: [
+          {
+            src: "*.pug",
+            cwd: "src/pug/",
+            dest: "./",
+            expand: true,
+            ext: ".html",
+          },
+        ],
+      },
     },
 
-    // sass task
+    // scss task
     sass: {
       dist: {
         options: {
-          style: 'inline'
+          style: "inline",
         },
-        files: [{
-          src: '*.sass',
-          cwd: 'src/sass/',
-          dest: 'dist/css',
-          expand: true,
-          ext: '.css'
-        }]
-      }
+        files: [
+          {
+            src: "*.scss",
+            cwd: "src/scss/",
+            dest: "dist/css",
+            expand: true,
+            ext: ".css",
+          },
+        ],
+      },
     },
 
     // uglify task (js minify)
     uglify: {
       my_target: {
         files: {
-          'dist/js/app.min.js': ['src/js/app.js']
-        }
-      }
+          "dist/js/app.min.js": ["src/js/app.js"],
+        },
+      },
     },
 
     // copy task (copy src/libraries to dist/libraries)
     copy: {
       libraries: {
         expand: true,
-        cwd: 'src',
-        src: ['libraries/**'],
-        dest: 'dist/'
+        cwd: "src",
+        src: ["libraries/**"],
+        dest: "dist/",
       },
       toroot: {
         expand: true,
-        cwd: 'src',
-        src: ['*.*'],
-        dest: './'
-      }
+        cwd: "src",
+        src: ["*.*"],
+        dest: "./",
+      },
     },
 
     // image compress task (compress all image src/images to dist/images)
-    imagemin : {
+    imagemin: {
       dynamic: {
-          files: [{
-              expand: true,
-              cwd: 'src/',
-              src: ['images/**/*.{png,jpg,gif}'],
-              dest: 'dist'
-          }]
-      }
+        files: [
+          {
+            expand: true,
+            cwd: "src/",
+            src: ["images/**/*.{png,jpg,gif}"],
+            dest: "dist",
+          },
+        ],
+      },
     },
     //minify css (only tun in production)
     cssmin: {
       target: {
-        files: [{
-          expand: true,
-          cwd: 'dist/css',
-          src: ['*.css', '!*.min.css'],
-          dest: 'dist/css',
-          ext: '.css'
-        }]
-      }
+        files: [
+          {
+            expand: true,
+            cwd: "dist/css",
+            src: ["*.css", "!*.min.css"],
+            dest: "dist/css",
+            ext: ".css",
+          },
+        ],
+      },
     },
     // auto refresh view on change in dist directory
     browserSync: {
       dev: {
         bsFiles: {
-            src : [
-                'dist/**.*',
-                '*.*'
-            ]
+          src: ["dist/**.*", "*.*"],
         },
         options: {
-            watchTask: true,
-            server: './'
-        }
-      }
+          watchTask: true,
+          server: "./",
+        },
+      },
     },
 
     // watch change inside directory to run task
     watch: {
       pug: {
-        files: ['src/pug/**/*.pug'],
-        tasks: ['pug']
+        files: ["src/pug/**/*.pug"],
+        tasks: ["pug"],
       },
       sass: {
-        files: ['src/sass/**/*.sass'],
-        tasks: ['sass']
+        files: ["src/scss/**/*.scss"],
+        tasks: ["scss"],
       },
       js: {
-        files: ['src/js/**/*.js'],
-        tasks: ['uglify']
+        files: ["src/js/**/*.js"],
+        tasks: ["uglify"],
       },
       copy: {
-        files: ['src/libraries/**', 'src/*.*'],
-        tasks: ['copy']
+        files: ["src/libraries/**", "src/*.*"],
+        tasks: ["copy"],
       },
       imagemin: {
-        files: ['src/images/**'],
-        tasks: ['imagemin']
-      }
-    }
+        files: ["src/images/**"],
+        tasks: ["imagemin"],
+      },
+    },
   });
 
   // initial
-  grunt.loadNpmTasks('grunt-contrib-pug');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-uglify')
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-browser-sync');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks("grunt-contrib-pug");
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-browser-sync");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-imagemin");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
 
   //register default task
-  if(process.env.NODE_ENV == 'production')
-  {
-    grunt.registerTask('default', ['pug', 'sass', 'uglify', 'copy', 'imagemin', 'cssmin'])
-  }else 
-  {
-    grunt.registerTask('default', ['pug', 'sass', 'uglify', 'copy', 'imagemin', 'browserSync', 'watch'])
+  if (process.env.NODE_ENV == "production") {
+    grunt.registerTask("default", [
+      "pug",
+      "sass",
+      "uglify",
+      "copy",
+      "imagemin",
+      "cssmin",
+    ]);
+  } else {
+    grunt.registerTask("default", [
+      "pug",
+      "sass",
+      "uglify",
+      "copy",
+      "imagemin",
+      "browserSync",
+      "watch",
+    ]);
   }
 };
